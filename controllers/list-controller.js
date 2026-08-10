74a7ccdcb2ed3ba8096460d21257d0a399181893
Rajashri <your-email@gmail.com>
2026-08-10 18:38:51 +0530

    cc

--- controllers/list-controller.js
+++ controllers/list-controller.js
@@ -1,7 +1,50 @@
-
 const Contact = require("../models/contact-model");
 const Career = require("../models/Career");
 const Newsletter = require("../models/Newsletter");
+const LocationEnquiry = require("../models/LocationEnquiry");
+
+const PopupEnquiry = require("../models/PopupEnquiry");
+
+const getPopupEnquiries = async (req, res) => {
+  try {
+    const enquiries = await PopupEnquiry.find().sort({
+      createdAt: -1,
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: enquiries,
+    });
+  } catch (error) {
+    console.error("Popup Enquiry Fetch Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+// GET ALL LOCATION ENQUIRIES
+const getLocationEnquiries = async (req, res) => {
+  try {
+    const enquiries = await LocationEnquiry.find().sort({
+      createdAt: -1,
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: enquiries,
+    });
+  } catch (error) {
+    console.error("Location Enquiry Fetch Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
 
 const getNewsletter = async (req, res) => {
   try {
@@ -18,7 +61,6 @@ const getNewsletter = async (req, res) => {
     });
   }
 };
-
 
 // GET ALL CONTACTS
 const getContacts = async (req, res) => {
@@ -54,7 +96,11 @@ const getCareers = async (req, res) => {
     });
   }
 };
+
 module.exports = {
   getCareers,
-  getContacts,getNewsletter
+  getContacts,
+  getNewsletter,
+  getLocationEnquiries,
+  getPopupEnquiries,
 };
--- router/list-router.js
+++ router/list-router.js
@@ -3,13 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 const {
-
-  getContacts,getCareers,
- getNewsletter
+  getContacts,
+  getCareers,
+  getNewsletter,
+  getLocationEnquiries,
+  getPopupEnquiries,
 } = require("../controllers/list-controller");
+router.get("/popup-enquiries", getPopupEnquiries);
 
 router.get("/contacts", getContacts);
 router.get("/careers", getCareers);
 router.get("/newsletter", getNewsletter);
-
+router.get("/location-enquiries", getLocationEnquiries);
 module.exports = router;
